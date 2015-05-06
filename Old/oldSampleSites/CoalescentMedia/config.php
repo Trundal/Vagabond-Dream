f49cb5ef4f68f9d58898e0e0de3a52e94d45fe37
Korey Powell <korey@vagabonddream.com>
2015-05-06 13:05:29 +0200

    Re-Commit

---
+++ Old/oldSampleSites/CoalescentMedia/config.php
@@ -0,0 +1,1 @@
+<?php// phpBB 2.x auto-generated config file// Do not change anything in this file!$dbms = 'mysql';$dbhost = 'mysql4.hostexcellence.com';$dbname = 'Ryphlin_BBtest';$dbuser = 'Ryphlin_ryphlin';$dbpasswd = 'Shaman';$table_prefix = 'phpbb_';define('PHPBB_INSTALLED', true);?>
